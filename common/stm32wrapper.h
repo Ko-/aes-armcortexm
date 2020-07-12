cd96c35ebd96f8904bc4c1c60d8b49b6eeac74bf
Ko- <k.stoffelen@cs.ru.nl>
2020-07-12 15:27:15 +0200

    Remove superfluous includes

--- common/stm32wrapper.h
+++ common/stm32wrapper.h
@@ -4,10 +4,8 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/usart.h>
-#include <libopencm3/cm3/nvic.h>
 #include <libopencm3/cm3/scs.h>
 #include <libopencm3/cm3/dwt.h>
-#include <libopencm3/stm32/dma.h>
 #include <libopencm3/stm32/flash.h>
 
 #ifdef NEEDS_RNG
